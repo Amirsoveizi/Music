25387c95901d13d2d7bd0502680c89c9fa4e7001
amir.soveizi <amirsoveizi2004@gmail.com>
2024-11-20 00:31:15 +0330

    save changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,27 +1,25 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     //hilt
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
 
+
 android {
     namespace = "com.a_s.musicplayer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.a_s.musicplayer"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -34,24 +32,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
+
 
 dependencies {
 
